a593730ca88f6d122e84436a0fbe237c154d434d
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-07 00:29:38 +0530

    Updated the root file

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const v1Router = require("./routes/index");
 const cors = require("cors")
-const jwt = require("jsonwebtoken")
 const app = express();
-const { JWT_SECRET } = require("./config");
 
 app.use(cors());
 
@@ -11,5 +9,9 @@ app.use(express.json());
 
 app.use("/api/v1", v1Router)
 
-app.listen(3000);
+const PORT = 3003;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
