64d4a02cee02e615374f022652a01c04717da4dd
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-07 23:20:58 +0530

    Added the dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,15 @@
+import { AppBar } from "../components/AppBar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/User";
+
 export function Dashboard() {
     return (
-        <>
-        </>
+        <div>
+            <AppBar />
+            <div className="m-8">
+                <Balance value={"10,000"} />
+                <Users />
+            </div>
+        </div>
     )
 }
