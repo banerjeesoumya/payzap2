fa20721db3c0ae923aaaa82a671c3fe2e96620e8
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-08 19:01:09 +0530

    Adding the App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom" 
 import './App.css'
+import { SignUp } from "./pages/SignUp"
+import { SignIn } from "./pages/SignIn"
+import { Dashboard } from "./pages/Dashboard"
+import { SendMoney } from "./pages/SendMoney"
 
 function App() {
   
   return (
     <>
-  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<SendMoney />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
