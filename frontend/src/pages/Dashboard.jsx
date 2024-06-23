ed670b51f660844e64366e9b62a4e0461203c0fc
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-23 11:56:47 +0530

    Fixed an issue with the Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,36 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { AppBar } from "../components/AppBar";
 import { Balance } from "../components/Balance";
 import { Users } from "../components/Users";
 
 export function Dashboard() {
-    return (
-        <div>
-            <AppBar />
-            <div className="m-8">
-                <Balance value />
-                <Users />
-            </div>
-        </div>
-    )
+  const navigate = useNavigate();
+  let memory = null;
+  if (localStorage.getItem("SignInToken") !== null) {
+    memory = localStorage.getItem("SignInToken");
+  } else {
+    memory = localStorage.getItem("SignUpToken");
+  }
+
+  useEffect(() => {
+    if (memory === null) {
+      alert("You are not logged into PayZap");
+      navigate("/signin");
+    }
+  }, [memory, navigate]);
+
+  if (memory === null) {
+    return null; 
+  }
+
+  return (
+    <div>
+      <AppBar />
+      <div className="m-8">
+        <Balance value /> 
+        <Users />
+      </div>
+    </div>
+  );
 }
